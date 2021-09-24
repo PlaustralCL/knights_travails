0a3bb73103ca1bc45bb452281eee0294bbe25962
PlaustralCL <plaustralcl@gmail.com>
2021-09-24 07:12:27 -0400

    Add instance variables to track position adjacent nodes

--- lib/square.rb
+++ lib/square.rb
@@ -3,9 +3,11 @@
 # Create an object to represent each square of the chessboard. This will
 # track the state of the individual squares.
 class Square
-  attr_accessor :name
+  attr_accessor :name, :position, :adjacent
 
   def initialize(name = "z0")
     @name = name
+    @position = [-1, -1]
+    @adjacent = []
   end
 end
