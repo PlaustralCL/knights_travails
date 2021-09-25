0441e22030ffa91e97ee6009732a0b91a528af3f
PlaustralCL <plaustralcl@gmail.com>
2021-09-25 15:22:20 -0400

    Limit test code availablity
    Only run the test code when the file is run directly from the
    command line. It will not run when called from another file.

--- lib/board.rb
+++ lib/board.rb
@@ -11,21 +11,25 @@ class Board
     asign_square_names
   end
 
+  # rubocop:todo Metrics/AbcSize
   def asign_square_names
     col_names = ("a".."h").to_a
     row_name = ("1".."8").to_a.reverse
     board.each_index do |col|
       board.each_index do |row|
         board[row][col].name = col_names[col] + row_name[row]
+        board[row][col].position = [row, col]
+
       end
     end
   end
+  # robocop:enable Metrics/AbcSize
 
   def show_board
     display_board = Array.new(8) { Array.new(8) }
     (0..7).each do |row|
       (0..7).each do |col|
-        display_board[row][col] = board[row][col].name
+        display_board[row][col] = board[row][col].position
       end
       p display_board[row]
     end
@@ -33,5 +37,7 @@ class Board
 
 end
 
-test = Board.new
-test.show_board
+if __FILE__ == $PROGRAM_NAME
+  test = Board.new
+  test.show_board
+end
