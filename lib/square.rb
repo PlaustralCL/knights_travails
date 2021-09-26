6cebf532ad80de6fc5026d3ba73d3d6a86dfc77f
PlaustralCL <plaustralcl@gmail.com>
2021-09-25 22:12:07 -0400

    Add more instance variables

--- lib/square.rb
+++ lib/square.rb
@@ -1,13 +1,16 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 # Create an object to represent each square of the chessboard. This will
 # track the state of the individual squares.
 class Square
-  attr_accessor :name, :position, :adjacent
+  attr_accessor :name, :position, :adjacent, :visited, :distance, :path
 
   def initialize(name = "z0")
     @name = name
     @position = [-1, -1]
     @adjacent = []
+    @visited = false
+    @distance = Float::INFINITY
+    @path = []
   end
 end
